e0d4f2f336e252e54b428927112d4120ab40c6e9
chenlu <chenlu@guahao.com>
2020-04-19 19:16:56 +0800

    jdbc驱动配置

--- src/main/java/com/sale/panda/config/OverStartEvent.java
+++ src/main/java/com/sale/panda/config/OverStartEvent.java
@@ -1,10 +1,10 @@
 package com.sale.panda.config;
 
+import java.io.IOException;
+
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-
-import java.io.IOException;
 
 /**
  * @author: chenlu
@@ -17,7 +17,7 @@ public class OverStartEvent {
     @EventListener({ApplicationReadyEvent.class})
     void applicationReadyEvent() {
         System.out.println("应用已经准备就绪 ... 启动浏览器");
-        String url = "http://localhost:8080/homePage";
+        String url = "http://127.0.0.1:8888/homePage";
         Runtime runtime = Runtime.getRuntime();
         try {
             runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
